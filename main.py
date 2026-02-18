eceb63a4cd331a81a070520a15923f7690acf3cd
Roman Krzmarzick <romankrzmarzick@gmail.com>
2026-02-18 17:40:54 +0100

    Update main.py
    Finished the logic for what combinations win or vice versa.

--- main.py
+++ main.py
@@ -12,44 +12,71 @@ class Game:
     def __init__(self):
         self.user_score = 0
         self.cpu_score = 0
-        self.cpu_move = []
+        self.cpu_move = ""
+        self.rounds = 0
+    def apply_score(self, val):
+        if val > 0:
+           self.user_score += val
+        if val < 0:
+            self.cpu_score += abs(val)
+        
 
-    def score_adder(self, val):
-        if val == True:
-           self.user_score += 1
-        if val is False:
-            self.cpu_score +=1
-
-    def cpu_num(self):
+    def cpu_pick(self):
         self.cpu_move = random.choice(GAME_MOVES)
 
-    def combination(self, user_move, cpu_move):
-        if user_move == cpu_move:
+    def combination(self, user_move):
+        if user_move == self.cpu_move:
             return None
-        if (user_move == "paper" and cpu_move == "rock") or (user_move == "rock" and cpu_move == "scissors") or (user_move == "scissors" and cpu_move == "paper"):
+        if (user_move == "paper" and self.cpu_move == "rock") or (user_move == "rock" and self.cpu_move == "scissors") or (user_move == "scissors" and self.cpu_move == "paper"):
             return True 
         else:
             return False
+    
+    def tiebreaker(self):
+        pass
 
+
+
+    def add_round(self):
+        self.rounds += 1
+
+
+
+    
+    
 def welcome_message():
-    print("Welcome to a classic game of RPSS. One match is three rounds; 2/3 of the rounds won = winner. |TWIST| If scores are tied, it goes until one side wins twice in a row!")
+    print("Welcome to a classic game of RPSS. One match is three rounds; if 2/3 of the rounds are won = winner. |TWIST| If scores are tied, one must win twice in a row!")
 
 def user_choice():
-    return Prompt.ask("What is your move going to be?", choices=GAME_MOVES, case_sensitive=False).lower().strip()
+    return Prompt.ask("What will your move be?", choices=GAME_MOVES)
+
+def round_message(val): 
+    print(f"| ROUND {val} |")
 
 def run():
     welcome_message()
-    while True:
-        game = Game()
+    game = Game()
+    for i in range(1, 4):
+        round_message(i)
+
+        game.cpu_pick()
+        print(f"Debugger {game.cpu_move}")
+        outcome = game.combination(user_choice())
+        print(f"Debugger {outcome}")
+        if outcome:
+            print("You Won")
+            game.apply_score(1)
         
-        game.cpu_num()
-        print(game.cpu_move)
-        answ = user_choice()
+        if outcome is False:
+            print("You Lost!")
+            game.apply_score(-1)
+        
+        if outcome is None:
+            print("You Tied!")
+        
+
     
-        print(answ)
-    
-        if answ == final_cpu:
-            print("hello world")
+      
        
        
        
