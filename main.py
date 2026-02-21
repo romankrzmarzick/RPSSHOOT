287da55c70986041d3f5c548d34a8bf71ae0ac1d
Roman Krzmarzick <romankrzmarzick@gmail.com>
2026-02-21 15:03:53 +0100

    Update main.py
    Added UI class for prints and inputs.

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from rich.prompt import Prompt
 from rich.console import Console
 from rich.prompt import Confirm
 
-console = Console()
-
-GAME_MOVES = ["rock", "paper", "scissors"]
+GAME_MOVES = ["rock", "paper", "scissors", "lizard", "spock"]
 
 class Game:
     
@@ -15,7 +13,6 @@ class Game:
         self.__class__.match_played()
         self.user_score = 0
         self.cpu_score = 0
-        self.cpu_move = ""
         
     @classmethod
     def match_played(cls):
@@ -28,14 +25,18 @@ class Game:
             self.cpu_score += 1
     
     def cpu_pick(self):
-        self.cpu_move = random.choice(GAME_MOVES)
+        return random.choice(GAME_MOVES)
 
-    def combination(self, user_move):
-        if user_move == self.cpu_move:
-            return None
-        if (user_move == "paper" and self.cpu_move == "rock") or (user_move == "rock" and self.cpu_move == "scissors") or (user_move == "scissors" and self.cpu_move == "paper"):
-            return True 
-        return False
+    def combination(self, user_move, cpu_move):
+        win_conditions = {
+            "rock" : {"scissors","lizard"},
+            "paper" : {"rock", "spock"},
+            "scissors " : {"paper", "lizard"},
+            "lizard" : {"spock", "paper"},
+            "spock" : {"rock", "scissors"}
+        }
+        if user_move == cpu_move: return None
+        return True if cpu_move in win_conditions[user_move] else False
     
     def isnot_tie(self):
         return self.user_score != self.cpu_score
@@ -43,38 +44,23 @@ class Game:
     def who_won(self):
        print("\nYOU WON THE MATCH!\n") if self.user_score > self.cpu_score else print("\nTHE COMPUTER DEFEATED YOU!\n")
     
-def welcome_message():
-    print("Welcome to RPSS! Each match is best of three — win 2 rounds to win the match. |TWIST|: If the match ends in a tie, the winner must have two more wins than losses.")
-
-def round_message(val): 
-    print(f"| ROUND {val} |")
-
-def user_choice():
-    return Prompt.ask("What will your move be?", choices=GAME_MOVES)
-
-def play_again():
-    return Confirm.ask("Play again?")
-
-def game_state(value):
-    if value == 0: return "THE MATCH IS TIED."
-    return "ONE MORE WIN AWAY!" if value > 0 else "THE COMPUTER ALMOST HAS YOU BEAT!"
     
 def run():
-    welcome_message()
+    ui = UI()
+    ui.welcome_message()
     while True:
         game = Game()
         for i in range(1, 4):
             if game.user_score == 2 or game.cpu_score == 2:
                 break
             
-            round_message(i)
+            ui.round_message(i)
 
-            game.cpu_pick()
-            user_move = user_choice()
-            outcome = game.combination(user_move)
-            input(f"You Chose {user_move} [E]".upper())
-            input("The Computer Chose... [E] ".upper())
-            print(game.cpu_move.upper())
+            cpu_move = game.cpu_pick()
+            user_move = ui.user_choice()
+            outcome = game.combination(user_move, cpu_move)
+            
+            ui.move_interface(user_move, cpu_move)
 
             if outcome is True:
                 print("Round Won!")
@@ -96,32 +82,78 @@ def run():
 
             while -2 < tie_score < 2:
                 if not_first_round: 
-                    print(game_state(tie_score))
+                    print(ui.game_state(tie_score))
                 
-                game.cpu_pick()
-                user_move = user_choice
-                tie_rounds = game.combination(user_move)
+                cpu_move = game.cpu_pick()
+                user_move = ui.user_choice()
+                tie_rounds = game.combination(user_move, cpu_move)
+                ui.move_interface(user_move, cpu_move)
 
-                input(f"You Chose {user_move} [E]".upper())
-                input("The Computer Chose... [E]".upper())
-                print(game.cpu_move.upper())
 
-                if tie_rounds:
+                if tie_rounds is True:
                     tie_score += 1
                     print("Round Won!")
                 elif tie_rounds is False:
                     print("Round Lost!")
                     tie_score -=1
-                elif tie_rounds is None:
+                else:
                     print("Round Tied!")
                 
                 not_first_round = True
 
             print("YOU WON AGAINST THE COMPUTER!") if tie_score > 0 else print("THE COMPUTER BEAT YOU!")
             
-        if not play_again():
-            print(f"THANKS FOR PLAYING! [Total Matches Played]: {Game.total_matches}")
+        if not ui.play_again():
+            
             break
     
 
 run()
+
+
+GAME_TEXT = {
+    "welcome_msg" : "Welcome to RPSS! Each match is best of three — win 2 rounds to win the match. |TWIST|: If the match ends in a tie, the winner must have two more wins than losses."
+    ""   
+}
+
+
+class UI:
+    def __init__(self):
+        self.cons = Console()
+        self.theme = {
+            "won" : "bold green",
+            "tie" : "yellow blink",
+            "lost" : "bold red",
+            "reg" : "white bold"
+        }
+    def welcome_message(self):
+        self.cons.print(GAME_TEXT["welcome_msg"], style=self.theme["reg"])
+           
+    
+    def user_choice(self):
+        return Prompt.ask("What will your move be?", choices=GAME_MOVES)
+    
+    def round_message(self, val): 
+        self.cons.print(f"| ROUND {val} |", style=self.theme["reg"])
+
+    def game_state(self, value):
+        if value == 0: return "THE MATCH IS TIED."
+        return "ONE MORE WIN AWAY!" if value > 0 else "THE COMPUTER ALMOST HAS YOU BEAT!"
+    
+    def good_bye(self):
+        print(f"THANKS FOR PLAYING! [Total Matches Played]: {Game.total_matches}")
+
+    def move_interface(self, user, cpu):
+        input(f"| You Chose {user} | [E]")
+        input(f"| The computer chose {cpu} | [E]")
+    
+    def play_again(self):
+        
+        return Confirm.ask("Play again?")
+    
+      
+def upper(func):
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        return output.upper()
+    return wrapper
